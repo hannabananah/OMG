c8bdeb50251349628dd270ea03dc646993b43664
godeeyeon <godeeyeon@gmail.com>
2024-09-27 13:12:19 +0900

    [#34] Feat: 주식 시장 컴포넌트 처리(3D 에셋 렌더링 및 클릭 처리)
    - done: 주식 시장 마켓 배경 이미지 & 눈 내리는 효과 적용
    - done: 3D 에셋 렌더링 및 애니메이션 적용 (트리 및 트리 장식 5개: socks-in-cane, cane, socks, reels, candy)
    - done: 트리 장식 클릭 시, 임의의 바구니 구역 내에 선택한 장식 넣어주기
    - done: 바구니에 있는 트리 장식 클릭 시, 선택 취소
    - done: 예외 처리(보유 현금 초과, 최대 거래 수량 초과)
    - done: 반복되는 3D 트리 장식 애니메이션 useFloatingObject 훅으로 분리

--- omg-front/src/components/stock-market/Socks.tsx
+++ omg-front/src/components/stock-market/Socks.tsx
@@ -1,8 +1,6 @@
-import { useRef } from 'react';
-
+import { useFloatingObject } from '@/hooks/useFloatingObject';
 import { useGLTF } from '@react-three/drei';
-import { ThreeEvent, useFrame } from '@react-three/fiber';
-import * as THREE from 'three';
+import { ThreeEvent } from '@react-three/fiber';
 
 interface SocksProps {
   onClick: (event: ThreeEvent<PointerEvent>) => void;
@@ -11,33 +9,21 @@ interface SocksProps {
 
 export default function Socks({ onClick, disabled }: SocksProps) {
   const { scene } = useGLTF('/models/socks/socks.gltf');
-  const ref = useRef<THREE.Object3D>(null);
 
-  let speed = 0.005;
-  let direction = 1;
+  const position = {
+    x: 2,
+    y: 0,
+    z: -2.3,
+  };
 
-  // 양말에 애니메이션 추가
-  useFrame(() => {
-    if (ref.current) {
-      // 회전 => 속도 좀 더 빠르게
-      ref.current.rotation.y += speed * 8;
-
-      // 위 아래 => 기본 속도
-      ref.current.position.y += direction * speed;
-
-      // 일정 범위 내에서 위 아래 방향을 전환
-      if (ref.current.position.y > 1.2 || ref.current.position.y < 1) {
-        direction *= -1; // 방향 전환
-      }
-    }
-  });
+  const ref = useFloatingObject(position.y);
 
   return (
     <>
       <primitive
         ref={ref} // useRef로 참조 설정
         object={scene}
-        position={[2, 1, -2.3]}
+        position={[position.x, position.y, position.z]}
         scale={[0.5, 0.5, 0.5]}
         rotation={[0, 0, 0]} // 초기 회전 상태
         onClick={onClick}
